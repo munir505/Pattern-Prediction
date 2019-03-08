ede7b98230b10ccb140d7afbf5ee60d52850794d
munir505 <31385912+munir505@users.noreply.github.com>
2019-03-08 11:29:06 +0000

    Update PatternPrediction.py

--- Pattern Prediction/PatternPrediction.py
+++ Pattern Prediction/PatternPrediction.py
@@ -1,13 +1,5 @@
 pattern_list = [0,4,8,12,16,20]
 
-#If we can find the difference between the numbers
-#Not just two of them, but all pairs, then we can split
-#To have different functions for different types
-
-#Ideally split to different types of patterns function
-#Such as linear patterns and fibonacci numbers
-#First we need to determine what type of pattern it is
-
 def linear_pattern (list_pattern):
     difference = find_difference(list_pattern[0], list_pattern[1])
     return pattern_list[len(pattern_list) - 1] + difference
@@ -34,8 +26,3 @@ def main():
     print(pattern_type(pattern_list))
 
 main()
-
-#So this is just the start
-#My plan is to accomidate as many different patterns as possible
-#This is currently only doing linear patterns
-#Also if you read these comments you may delete them
